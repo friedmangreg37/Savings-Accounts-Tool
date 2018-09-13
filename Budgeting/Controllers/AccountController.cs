77a3e943af9fb4b1ebeccb1eefe17decbd971648
Greg Friedman <8547788+friedmangreg37@users.noreply.github.com>
2018-09-13 15:33:51 -0400

    Added mdf file back in to get DB working again

--- Budgeting/Controllers/AccountController.cs
+++ Budgeting/Controllers/AccountController.cs
@@ -137,7 +137,7 @@ namespace Budgeting.Controllers
         [AllowAnonymous]
         public ActionResult Register()
         {
-            return HttpNotFound();
+            //return HttpNotFound();
             return View();
         }
 
@@ -148,7 +148,7 @@ namespace Budgeting.Controllers
         [ValidateAntiForgeryToken]
         public async Task<ActionResult> Register(RegisterViewModel model)
         {
-            return HttpNotFound();
+            //return HttpNotFound();
             if (ModelState.IsValid)
             {
                 var user = new ApplicationUser { UserName = model.Username, Email = model.Email };
