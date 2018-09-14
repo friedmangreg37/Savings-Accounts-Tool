4c8a53fa9b974b781239aa049bfd87ab62aee944
Greg Friedman <8547788+friedmangreg37@users.noreply.github.com>
2018-09-14 09:10:18 -0400

    Updated tests for HomeController

--- Budgeting.UnitFixtures/Controllers/TestHomeController.cs
+++ Budgeting.UnitFixtures/Controllers/TestHomeController.cs
@@ -10,40 +10,33 @@ namespace Budgeting.UnitFixtures.Controllers
         [Test]
         public void Index()
         {
-            // Arrange
             HomeController controller = new HomeController();
 
-            // Act
             ViewResult result = controller.Index() as ViewResult;
 
-            // Assert
             Assert.IsNotNull(result);
         }
 
         [Test]
         public void About()
         {
-            // Arrange
             HomeController controller = new HomeController();
 
-            // Act
             ViewResult result = controller.About() as ViewResult;
 
-            // Assert
+            Assert.IsNotNull(result);
             Assert.AreEqual("Your application description page.", result.ViewBag.Message);
         }
 
         [Test]
         public void Contact()
         {
-            // Arrange
             HomeController controller = new HomeController();
 
-            // Act
             ViewResult result = controller.Contact() as ViewResult;
 
-            // Assert
             Assert.IsNotNull(result);
+            Assert.AreEqual("Your contact page.", result.ViewBag.Message);
         }
     }
 }
