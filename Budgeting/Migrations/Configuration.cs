6a2f189fd2c05925962dc50cbcf6e047bb51ddc8
Greg Friedman <8547788+friedmangreg37@users.noreply.github.com>
2019-04-01 11:52:50 -0400

    Set AutomaticMigrationDataLossAllowed to true

--- Budgeting/Migrations/Configuration.cs
+++ Budgeting/Migrations/Configuration.cs
@@ -1,16 +1,14 @@
 namespace Budgeting.Migrations
 {
-    using Budgeting.Models;
-    using Microsoft.AspNet.Identity;
-    using Microsoft.AspNet.Identity.EntityFramework;
+    using Models;
     using System.Data.Entity.Migrations;
-    using System.Linq;
 
     internal sealed class Configuration : DbMigrationsConfiguration<ApplicationDbContext>
     {
         public Configuration()
         {
             AutomaticMigrationsEnabled = true;
+            AutomaticMigrationDataLossAllowed = true;
             ContextKey = "Budgeting.Models.ApplicationDbContext";
         }
 
